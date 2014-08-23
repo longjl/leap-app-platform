bfb3cda5f3973e84099232f7e167d02fe37ee817
longjl <longjl@anbi.tv>
2014-08-23 12:17:43 +0800

    add file

---
+++ apps/video-leap-app/src/main/java/cn/leap/app/MainActivity.java
@@ -0,0 +1,18 @@
+package cn.leap.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by longjianlin on 14-8-23.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+
+        super.onCreate(bundle);
+
+        setContentView(R.layout.main);
+    }
+}
